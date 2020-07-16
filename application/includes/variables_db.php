423eb892b4075edc70eb2b7cab04e6e365a8128e
Erick Nunez <aerick.nunez@gmail.com>
2020-07-15 23:09:48 -0600

    Update variables_db.php

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -8,7 +8,10 @@ define("USER", "superpol_erick"); 			// The database username.
 define("PASSWORD", "caca007125-"); 	// The database password.
 define("DATABASE", "superpol_justomarket");
 define("BASE_URL", "https://justomarket.com/");  
-define("HOST_URL", "https://");  
+define("HOST_URL", "https://");
+
+define("URL_SERVER", "https://pizto.com/login/"); 
+define("TD_SERVER", "10");    
 } else {
 
 define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
@@ -16,7 +19,10 @@ define("USER", "root"); 			// The database username.
 define("PASSWORD", "erick"); 	// The database password. 
 define("DATABASE", "justomarket"); 
 define("BASE_URL", "http://localhost/justomarket/");
-define("HOST_URL", "http://");  
+define("HOST_URL", "http://"); 
+
+define("URL_SERVER", "https://localhost/cozto/"); 
+define("TD_SERVER", "10");  
 }
 
 define("CAN_REGISTER", "any");
@@ -24,7 +30,4 @@ define("DEFAULT_ROLE", "member");
 define("SECURE", FALSE);    // For development purposes only!!!!
 
 define("BASEPATH", "https://justomarket.com/");  
-
-define("URL_SERVER", "http://localhost/cozto/"); 
-define("TD_SERVER", "10"); 
 ?>
