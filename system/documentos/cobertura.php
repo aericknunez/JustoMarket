0c839233e6d19a2c267d25a2bdb9ea4635c89d1b
Erick Nunez <aerick.nunez@gmail.com>
2020-09-02 17:20:19 -0600

    Mapa

--- system/documentos/cobertura.php
+++ system/documentos/cobertura.php
@@ -9,7 +9,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <h4 class="vino"><strong>ZONAS DE COBERTURA</strong></h4>
 
-<iframe src="https://www.google.com/maps/d/embed?mid=1Fuj189S0eINiEGotbtceG7y2MEPZCZk5" width="1024" height="480"></iframe>
 
+
+
+<!--Google map-->
+<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+  <iframe src="https://www.google.com/maps/d/embed?mid=1Fuj189S0eINiEGotbtceG7y2MEPZCZk5" frameborder="0"
+    style="border:0" allowfullscreen></iframe>
+</div>
+
+<!--Google Maps-->
 
 </div>
