850790cfbb130dde02a663026fde18fbe78c51b3
Erick Nunez <aerick.nunez@gmail.com>
2020-09-10 01:09:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $db = new dbConn();
 $seslog = new Login();
 $seslog->sec_session_start();
 
-print_r($_SESSION);
+// print_r($_SESSION);
 
 
 $_SESSION["last_url"] = HOST_URL . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
