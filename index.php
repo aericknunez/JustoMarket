68efd4ca751f3fd29db22931e1993864f36ae9b6
Erick Nunez <aerick.nunez@gmail.com>
2020-09-11 22:15:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ if($_SESSION["mayordeedad"] == FALSE){
 
 
 echo '<p style="color: red; background: black; font-size: 18px; text-align: center;">ESTAMOS REALIZANDO PRUEBAS EN ESTE MOMENTO ES PROBABLE QUE EL SISTEMA ESTE FALLANDO</p>';
-print_r($_SESSION);
 
 
 $_SESSION["last_url"] = HOST_URL . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
