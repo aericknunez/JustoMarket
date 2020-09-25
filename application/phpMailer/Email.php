9ebbff1f07240fd24cfe07303cce22e75f9c405a
Erick Nunez <aerick.nunez@gmail.com>
2020-09-24 18:53:23 -0600

    Update Email.php

--- application/phpMailer/Email.php
+++ application/phpMailer/Email.php
@@ -40,7 +40,7 @@ $mail = new PHPMailer(true);
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'no-reply@justomarket.com';                     // SMTP username
     $mail->Password   = 'caca007125-';                               // SMTP password
-    $mail->SMTPSecure = "SSL";         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
