2b6718fad32abb5269a9a569b59d574f815196e8
Erick Nunez <aerick.nunez@gmail.com>
2020-12-17 12:00:08 -0600

    Update preguntas.php

--- system/documentos/preguntas.php
+++ system/documentos/preguntas.php
@@ -9,7 +9,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <h4 class="vino"><strong>PREGUNTAS FRECUENTES</strong></h4>
 <br>
-<div class="vino"><strong>¿Cuál es el monto mínimo de compra?</strong></div>
+
+
+<div class="vino"><strong>¿Cuál es la dirección? ¿Dónde se encuentran ubicados? </strong></div>
+Puedes visitarnos en 10a. avenida norte #1-2 Bo. El Ángel, Sonsonate, Sonsonate. Frente al Complejo Educativo Tercer Ciclo Thomas Jefferson. 
+<br><br>
+
+<div class="vino"><strong>¿Cuál es el monto mínimo de compra, cobertura, costo de envío, tiempo de entrega? </strong></div>
+El monto mínimo de compra, costo y tiempo de entrega estimado depende del municipio de entrega. Puedes consultar en: <a href="https://justomarket.com/cobertura ">https://justomarket.com/cobertura </a>
+<br><br>
+
+<div class="vino"><strong>¿Tienen servicio a domicilio? ¿Hay cobertura en mi zona de residencia? </strong></div>
+Hacemos entregas a domicilio en todo el departamento de Sonsonate. Cuando ingreses tus datos de envío, la plataforma te indicará si hay servicio en tu zona.
+<br><br>
+
+<div class="vino"><strong>¿Cuál es la forma de pago? | ¿Puedo pagar en efectivo? </strong></div>
+Puedes pagar en efectivo y con tarjetas de crédito y débito*, ya sea con link de pago (paylink) o cuando llegue el repartidor mediante POS inalámbrico. Es totalmente seguro, fácil y rápido. *Aceptamos tarjetas Visa y MasterCard. 
+<br><br>
+
+<div class="vino"><strong>¿Puedo hacer pedidos desde el exterior de El Salvador? </strong></div>
+Sí, tenemos servicio en línea y puedes hacerlo desde cualquier lugar del mundo. 
+<br><br>
+
+<div class="vino"><strong>Olvidé mi contraseña de usuario </strong></div>
+En la pantalla de inicio de sesión, haz clic en "olvidé mi contraseña", introduce tu dirección de correo registrado y el sistema te enviará un mensaje con un enlace para cambiarla. 
+<br><br>
+
+
+<div class="vino"><strong>¿Cómo sé si aceptaron mi registro de datos y si mi orden fue aceptada? </strong></div>
+Se envía un correo electrónico automático para confirmar tu registro y cuando la orden se realizó con éxito. 
+<br><br>
+
+<div class="vino"><strong>Para comprar ¿Es necesario registrarse? </strong></div>
+No, puedes comprar como invitado. 
+<br><br>
+
+<div class="vino"><strong>¿Van a llamarme para notificarme la entrega de mi pedido? </strong></div>
+Te llamaremos para confirmar que hemos recibido tu pedido y verificar la dirección de entrega y verificar los productos que solicitaste.
+<br><br>
+
+
+
+<!-- <div class="vino"><strong>¿Cuál es el monto mínimo de compra?</strong></div>
 El monto mínimo de compra es de $15, para retiro en sala y a domicilio.
 <br><br>
 <div class="vino"><strong>¿Cuál es la forma de pago? | ¿Puedo pagar en efectivo?</strong></div>
@@ -51,6 +92,6 @@ Cuando ingreses tus datos de envío, la plataforma te indicará si hay servicio 
 <div class="vino"><strong>¿Van a llamarme para notificarme la entrega de mi pedido?</strong></div>
 
 Te llamaremos únicamente si necesitemos completar tu pedido, ya sea cambiando o sustituyendo productos de tu lista por similares o si necesitemos verificar la dirección de tu domicilio.
-<br><br>
+<br><br> -->
 
 </div>
