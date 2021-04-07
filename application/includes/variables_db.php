288f495b713cffc58b03020cb6092ab2f4189410
aericknunez <aerick.nunez@gmail.com>
2021-04-06 20:41:11 -0600

    Update variables_db.php

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -3,14 +3,14 @@ date_default_timezone_set('America/El_Salvador');
 
 if(Helpers::ServerDomain() == TRUE){
 
-define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
-define("USER", "superpol_erick"); 			// The database username. 
-define("PASSWORD", "caca007125-"); 	// The database password.
-define("DATABASE", "superpol_justomarket");
-define("BASE_URL", "https://justomarket.com/");  
-define("HOST_URL", "https://");
+define("HOST", "db5002171255.hosting-data.io"); 			//35.225.56.157 The host you want to connect to. 
+define("USER", "dbu60825"); 			// The database username. 
+define("PASSWORD", "Caca007125-"); 	// The database password.
+define("DATABASE", "dbs1756640");
+define("BASE_URL", "http://justomarket.com/");  
+define("HOST_URL", "http://");
 
-define("URL_SERVER", "https://pizto.com/login/"); 
+define("URL_SERVER", "https://sistema.hibridosv.com"); 
 define("TD_SERVER", "10");    
 } else {
 
@@ -29,5 +29,5 @@ define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
 define("SECURE", FALSE);    // For development purposes only!!!!
 
-define("BASEPATH", "https://justomarket.com/");  
+define("BASEPATH", "http://justomarket.com/");  
 ?>
